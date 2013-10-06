ba94671c76fdf012c07d863bb0faa7e059c7cbb9
Robert Virding <rvirding@gmail.com>
2013-10-06 18:58:07 +0200

    Remove user_pl.erl as it has been subsumed by erlog_lists

--- src/erlog_lists.erl
+++ src/erlog_lists.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2008-2013 Robert Virding
+%% Copyright (c) 2013 Robert Virding
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -18,7 +18,8 @@
 %% 
 %% This is a standard lists library for Erlog. Everything here is
 %% pretty basic and common to most Prologs. We are experimenting here
-%% and some predicates are compiled.
+%% and some predicates are compiled. We only get a small benefit when
+%% only implementing indexing on the first argument.
 
 -module(erlog_lists).
 
@@ -28,7 +29,7 @@
 -export([load/1]).
 
 %% Library functions.
--export([append_3/6,insert_3/6,member_2/6]).
+-export([append_3/6,insert_3/6,member_2/6,reverse_2/6]).
 
 %%-compile(export_all).
 
@@ -50,6 +51,7 @@ load(Db0) ->
     Db1 = foldl(fun ({Head,M,F}, Db) ->
 			add_compiled_proc(Head, M, F, Db) end, Db0,
 		[
+		 {{reverse,2},?MODULE,reverse_2},
 		 {{member,2},?MODULE,member_2},
 		 {{insert,3},?MODULE,insert_3},
 		 {{append,3},?MODULE,append_3}
@@ -60,12 +62,6 @@ load(Db0) ->
 	   %% insert(L, X, [X|L]). insert([H|L], X, [H|L1]) :- insert(L, X, L1).
 	   %% delete([X|L], X, L). delete([H|L], X, [H|L1]) :- delete(L, X, L1).
 	   {':-',{delete,{1},{2},{3}},{insert,{3},{2},{1}}},
-	   %% member(X, [X|_]). member(X, [_|T]) :- member(X, T).
-	   %% reverse([], []).
-	   %% reverse([H|L1], L) :- reverse(L1, L2), append(L2, [H], L).
-	   {reverse,[],[]},
-	   {':-',{reverse,[{1}|{2}],{3}},
-	    {',',{reverse,{2},{4}},{append,{4},[{1}],{3}}}},
 	   %% perm([], []).
 	   %% perm([X|Xs], Ys1) :- perm(Xs, Ys), insert(Ys, X, Ys1).
 	   {perm,[],[]},
@@ -139,3 +135,44 @@ fail_member_2(#cp{next=Next0,bs=Bs,vn=Vn}, Cps, Db, A1, A2) ->
     T = {Vn+1},
     Next1 = [{member,A1,T}|Next0],
     unify_prove_body(A2, [H|T], Next1, Cps, Bs, Vn+2, Db).
+
+%% reverse_2(Head, NextGoal, Choicepoints, Bindings, VarNum, Database) -> void.
+%% reverse([], []).
+%% reverse([H|L1], L) :- reverse(L1, L2), append(L2, [H], L).
+
+reverse_2(Head, Next0, Cps, Bs0, Vn, Db) ->
+    case dderef(Head, Bs0) of
+%% 	{reverse,[],[]} -> prove_body(Next0, Cps, Bs0, Vn, Db);
+%% 	{reverse,[],{_}=Var} ->
+%% 	    Bs1 = add_binding(Var, [], Bs0),
+%% 	    prove_body(Next0, Cps, Bs1, Vn, Db);
+%% 	{reverse,[],_} -> fail(Cps, Db);
+	{reverse,[],A2} ->
+	    unify_prove_body(A2, [], Next0, Cps, Bs0, Vn, Db);
+	{reverse,[H|T],A2} ->
+	    L = {Vn},
+	    L1 = A2,
+	    %%Next1 = [{reverse,T,L},{append,L,[H],L1}|Next0],
+	    %%prove_body(Next1, Cps, Bs0, Vn+1, Db);
+	    Next1 = [{append,L,[H],L1}|Next0],
+	    reverse_2({reverse,T,L}, Next1, Cps, Bs0, Vn+1, Db);
+	{reverse,{_}=A1,A2} ->
+	    FailFun = fun (LCp, LCps, LDb) ->
+			      fail_reverse_2(LCp, LCps, LDb, A1, A2)
+		      end,
+	    Cp = #cp{type=compiled,data=FailFun,next=Next0,bs=Bs0,vn=Vn},
+	    Bs1 = add_binding(A1, [], Bs0),
+	    unify_prove_body(A2, [], Next0, [Cp|Cps], Bs1, Vn, Db);
+	{reverse,_A1,_A2} -> fail(Cps, Db)
+    end.
+
+fail_reverse_2(#cp{next=Next,bs=Bs0,vn=Vn}, Cps, Db, A1, A2) ->
+    H = {Vn},
+    T = {Vn+1},
+    L1 = A2,
+    L = {Vn+2},
+    Bs1 = add_binding(A1, [H|T], Bs0),
+    %%Next1 = [{reverse,T,L},{apperse,L,[H],L1}|Next],
+    %%prove_body(Next1, Cps, Bs1, Vn+3, Db).
+    Next1 = [{append,L,[H],L1}|Next],
+    reverse_2({reverse,T,L}, Next1, Cps, Bs1, Vn+3, Db).
