9e9e1a9cb640351b824664a61d115b294762ca23
Zachary Kessin <zkessin@gmail.com>
2014-05-15 14:50:47 +0300

    number test

---
+++ test/erlog_bips_tests.erl
@@ -0,0 +1,67 @@
+-module(erlog_bips_tests).
+-include_lib("eqc/include/eqc.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+prop_float()->
+    ?FORALL(I,real(),
+            begin
+                {ok, PID}    = erlog:start_link(),
+                {succeed, _} = erlog:prove(PID, {float, I}),
+                true
+            end).
+
+prop_integer()->
+    ?FORALL(I,int(),
+            begin
+                {ok, PID}    = erlog:start_link(),
+                {succeed, _} = erlog:prove(PID, {integer, I}),
+                true
+            end).
+prop_number()->
+    ?FORALL(I,oneof([int(),real()]),
+            begin
+                {ok, PID}    = erlog:start_link(),
+                {succeed, _} = erlog:prove(PID, {number, I}),
+                true
+            end).
+
+any() ->
+    oneof([int(),
+           real(),
+           char(),
+           binary(),
+           bitstring(),
+           bool()
+           ]).
+
+prop_compound() ->
+    ?FORALL(V,
+            oneof([list(any()),
+                   {any()},
+                   {any(), any()},
+                   {any(), any(), any()},
+                   {any(), any(), any(), any()}
+                  ]),
+            begin
+                {ok, PID}    = erlog:start_link(),
+                {succeed, _} = erlog:prove(PID, {compound, V}),
+                true
+            end).
+       
+
+out(P) ->
+   on_output(fun(S,F) -> io:format(user, S, F) end,P).
+
+run_test_() ->
+    Props = [
+             fun prop_integer/0,
+             fun prop_number/0,
+             fun prop_float/0
+%             fun prop_compound/0
+             ],    
+    [
+     begin
+         P = out(Prop()),
+         ?_assert(quickcheck(numtests(500,P)))
+     end
+     || Prop <- Props].
