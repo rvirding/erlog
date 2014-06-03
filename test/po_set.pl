8732a2199f8c05c284511653bc6411bb854a82ab
Zachary Kessin <zkessin@gmail.com>
2014-06-03 17:44:27 +0300

    some code for partially ordered sets

---
+++ test/po_set.pl
@@ -0,0 +1,37 @@
+/* -*-Prolog -*- */
+
+
+edge(a,b).
+edge(a,c).
+edge(c,d).
+edge(b,e).
+edge(d,f).
+edge(b,f).
+
+connected(A,B) :-
+	edge(A,B).
+
+sib(A,B) :-
+	path(C,A,_),
+	path(C,B,_),
+	\+path(A,B,_).
+
+ancestor(A,B) :-
+	path(A,B,_).
+
+descendent(A,B) :-
+	path(B,A,_).
+
+path(A,B,Path) :-
+       travel(A,B,[A],Q), 
+       reverse(Q,Path).
+
+travel(A,B,P,[B|P]) :- 
+       connected(A,B).
+travel(A,B,Visited,Path) :-
+       connected(A,C),           
+       C \== B,
+       \+member(C,Visited),
+       travel(C,B,[C|Visited],Path). 
+
+
