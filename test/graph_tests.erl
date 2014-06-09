bbd1cfbceaa598d95a6ae21e25051ff3d9a1413e
Zachary Kessin <zkessin@gmail.com>
2014-06-09 21:31:42 +0200

    add timeout

--- test/graph_tests.erl
+++ test/graph_tests.erl
@@ -20,22 +20,24 @@ gnode() ->
 gnodes() ->
     non_empty(list(gnode())).
 
+%%TODO ADD TIMEOUT HERE
 prop_travel() ->
         ?FORALL({Nodes},
-            {gnodes()},
-            begin
-                {ok, PID}   = erlog:start_link(),
-		ok          = erlog:consult(PID, "../test/graph.pl"),
-		[erlog:prove(PID, {assertz, Node})|| Node <- Nodes],
-
-		true = lists:all(fun({edge,Start,_})->
-					 {succeed, R} = erlog:prove(PID, {path, Start, {'End'},{'Path'}}),
-					 End  = proplists:get_value('End',  R),
-					 Path = proplists:get_value('Path', R ),
-					 {succeed, []} =:= erlog:prove(PID, {path, Start, End, Path})
-
-			  end, Nodes)
-		end).
+		{gnodes()},
+		?TIMEOUT(1000,
+			 begin
+			     {ok, PID}   = erlog:start_link(),
+			     ok          = erlog:consult(PID, "../test/graph.pl"),
+			     [erlog:prove(PID, {assertz, Node})|| Node <- Nodes],
+			     
+			     true = lists:all(fun({edge,Start,_})->
+						      {succeed, R} = erlog:prove(PID, {path, Start, {'End'},{'Path'}}),
+						      End  = proplists:get_value('End',  R),
+						      Path = proplists:get_value('Path', R ),
+						      {succeed, []} =:= erlog:prove(PID, {path, Start, End, Path})
+					      
+					      end, Nodes)
+			 end)).
 
 
 out(P) ->
@@ -48,7 +50,7 @@ run_test_() ->
     [
      begin
          P = out(Prop()),
-         ?_assert(quickcheck(numtests(250,P)))
+         ?_assert(quickcheck(numtests(500,P)))
      end
      || Prop <- Props].
 
