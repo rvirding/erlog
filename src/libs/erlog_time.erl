a8257ae1216731c482f526f3a0217a4ee8b286ff
Valery Tikhonov <com.tihon@mail.ru>
2014-07-18 22:33:13 +0000

    removed debug

--- src/libs/erlog_time.erl
+++ src/libs/erlog_time.erl
@@ -60,7 +60,6 @@ datediff_4({date_diff, TS1, TS2, Format, Res}, Params = #param{next_goal = Next,
 
 %% Adds number of seconds T2 in Type format to Time1. Returns timestamp
 add_time_4({add_time, Time1, Type, T2, Res}, Params = #param{next_goal = Next, bindings = Bs0}) ->
-	io:format("add time ~p ~p ~p~n", [Time1, Type, T2]),
 	Diff = check_var(Time1, Bs0) + date_to_seconds(check_var(T2, Bs0), Type),
 	Bs = ec_support:add_binding(Res, Diff, Bs0),
 	ec_body:prove_body(Params#param{goal = Next, bindings = Bs}).
