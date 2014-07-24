b43734529ebd705aa640c7c05f059f4d8bc4a666
Valery Tikhonov <com.tihon@mail.ru>
2014-07-24 21:02:52 +0000

    fix concatination in case of string results

---
+++ src/libs/external/currency/erlog_curr_sup.erl
@@ -0,0 +1,76 @@
+%%%-------------------------------------------------------------------
+%%% @author tihon
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 24. Июль 2014 20:10
+%%%-------------------------------------------------------------------
+-module(erlog_curr_sup).
+-author("tihon").
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link() ->
+	{ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+	{ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
+		MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
+		[ChildSpec :: supervisor:child_spec()]
+	}} |
+	ignore |
+	{error, Reason :: term()}).
+init([]) ->
+	RestartStrategy = one_for_one,
+	MaxRestarts = 1000,
+	MaxSecondsBetweenRestarts = 3600,
+
+	SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+	Restart = permanent,
+	Shutdown = 2000,
+	Type = worker,
+
+	AChild = {'AName', {'AModule', start_link, []},
+		Restart, Shutdown, Type, ['AModule']},
+
+	{ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
