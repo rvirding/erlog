5aacec84efb068a7848045b7936962415c32f8e8
Zachary Kessin <zkessin@gmail.com>
2014-08-26 09:12:33 +0300

    updated test to new format

--- test/erlog_halt_tests.erl
+++ test/erlog_halt_tests.erl
@@ -5,8 +5,8 @@
 
 erlog_halt_test() ->
     {Pid,Ref} = spawn_monitor(fun() ->
-				      Erlog = erlog:new(),
-				      ?debugVal(Erlog({prove, {halt, test}})),
+				      {ok,Erlog} = erlog:new(),
+				      erlog:prove(Erlog,{halt, test}),
 				      timer:sleep(300),
 				      ok
 				end),
