567ae398932b66e4ffde3bfb34a52023e5518583
Valery Tikhonov <com.tihon@mail.ru>
2014-11-18 00:00:56 +0000

    fix merge

--- src/libs/standard/time/main/erlog_time.erl
+++ src/libs/standard/time/main/erlog_time.erl
@@ -59,8 +59,8 @@ prove_goal(Params = #param{goal = {date_diff, _, _, _, _} = Goal, next_goal = Ne
   {date_diff, TS1, TS2, Format, Res} = erlog_ec_support:deref(Goal, Bs0),
   case check_bound([TS1, TS2, Format]) of
     ok ->
-      Diff = timer:now_diff(erlog_et_logic:ts_to_date(erlog_et_logic:check_var(TS1, Bs0)), erlog_et_logic:ts_to_date(erlog_et_logic:check_var(TS2, Bs0))) / 1000000,
-      Time = erlog_et_logic:seconds_to_date(Diff, erlog_et_logic:check_var(Format, Bs0)),
+      Diff = timer:now_diff(erlog_et_logic:ts_to_date(erlog_ec_support:check_var(TS1, Bs0)), erlog_et_logic:ts_to_date(erlog_ec_support:check_var(TS2, Bs0))) / 1000000,
+      Time = erlog_et_logic:seconds_to_date(Diff, erlog_ec_support:check_var(Format, Bs0)),
       case erlog_ec_support:try_add(Res, Time, Bs0) of
         error -> erlog_errors:fail(Params);
         Bs -> erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs})
