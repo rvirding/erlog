e6653fc31162664f969e08d7d77e444c228d44e7
Valery Tikhonov <com.tihon@mail.ru>
2014-12-11 22:58:15 +0000

    improve concat

--- src/libs/standard/string/erlog_string.erl
+++ src/libs/standard/string/erlog_string.erl
@@ -19,12 +19,14 @@
 
 
 load(DbState) ->
-  lists:foldl(fun(Head, UDBState) -> erlog_memory:load_kernel_space(UDBState, ?MODULE, Head) end, DbState, ?ERLOG_STRING).
+  lists:foldl(fun(Head, UDBState) ->
+    erlog_memory:load_kernel_space(UDBState, ?MODULE, Head) end, DbState, ?ERLOG_STRING).
 
 prove_goal(Params = #param{goal = {concat, Strings, Res}, next_goal = Next, bindings = Bs0}) ->
   case erlog_ec_support:dderef_list(Strings, Bs0) of
     List when is_list(List) ->
-      Bs1 = erlog_ec_support:add_binding(Res, lists:concat(List), Bs0),
+      ConcatMe = lists:foldr(fun preprocess_concat/2, [], List),
+      Bs1 = erlog_ec_support:add_binding(Res, ConcatMe, Bs0),
       erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs1});
     _ -> erlog_errors:fail(Params)
   end;
@@ -45,3 +47,12 @@ prove_goal(Params = #param{goal = {split, _, _, _} = Goal, next_goal = Next, bin
   List = string:tokens(Str, Del),
   Bs1 = erlog_ec_support:add_binding(Res, List, Bs0),
   erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs1}).
+
+
+%% @private
+preprocess_concat(Object, Acc) when is_list(Object); is_tuple(Object) ->
+  case io_lib:printable_list(Object) of
+    true -> [Object | Acc];
+    false -> [lists:flatten(io_lib:format("~p", [Object])) | Acc]
+  end;
+preprocess_concat(Object, Acc) -> [Object | Acc].
