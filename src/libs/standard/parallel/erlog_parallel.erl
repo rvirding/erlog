0c0997af3c70510fde79ff04ee529bfd1213dc30
Valery Tikhonov <com.tihon@mail.ru>
2015-02-02 21:41:59 +0000

    improve parallel interface

--- src/libs/standard/parallel/erlog_parallel.erl
+++ src/libs/standard/parallel/erlog_parallel.erl
@@ -33,16 +33,19 @@ prove_goal(Params = #param{goal = {spawn, _, _} = G, next_goal = Next, bindings 
   erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs1});
 prove_goal(Params = #param{goal = {join, _, _} = G, next_goal = Next, bindings = Bs0}) ->
   {join, Pid, Timeout} = erlog_ec_support:dderef(G, Bs0),
-  join(Pid, Timeout),
-  erlog_ec_core:prove_body(Params#param{goal = Next});
+  case catch join(Pid, Timeout) of
+    ok -> erlog_ec_core:prove_body(Params#param{goal = Next});
+    _ -> erlog_errors:fail(Params)
+  end;
 prove_goal(Params = #param{goal = {check, _, _} = G, next_goal = Next, bindings = Bs0, var_num = Vn0}) ->
   {check, Pid, Result} = erlog_ec_support:dderef(G, Bs0),
   case recv_res(Pid, 0) of
     empty ->
-      Bs1 = erlog_ec_support:add_binding(Result, not_ready, Bs0),
+      Bs1 = erlog_ec_support:add_binding(Result, false, Bs0),
       erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs1});
     {succeed, _, Bs1, Vn1, _} ->
-      erlog_ec_core:prove_body(Params#param{goal = Next, bindings = dict:merge(fun merge_dicts/3, Bs0, Bs1), var_num = Vn0 + Vn1});
+      Bs = erlog_ec_support:add_binding(Result, true, dict:merge(fun merge_dicts/3, Bs0, Bs1)),
+      erlog_ec_core:prove_body(Params#param{goal = Next, bindings = Bs, var_num = Vn0 + Vn1});
     _ -> erlog_errors:fail(Params)
   end.
 
@@ -54,7 +57,7 @@ merge_dicts(_, _, Value2) -> Value2.
 join_proc(Pid, TM) ->
   receive
     {Pid, finish} -> ok
-  after TM -> empty
+  after TM -> throw(timeout)
   end.
 
 %% @private
